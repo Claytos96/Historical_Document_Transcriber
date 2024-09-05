9dbd1fde2d68c0392cde440005e0fab03d3a9f7e
Hugh Clayton <hughgrassbyclayton@hotmail.com>
2024-09-05 15:49:53 +1000

    add document model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_05_050259) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_05_053906) do
+  create_table "documents", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "text"
+    t.string "image"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_documents_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
@@ -18,4 +29,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_05_050259) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "documents", "users"
 end
